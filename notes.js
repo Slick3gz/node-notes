e20b6eeb8e654a3fa5c68059cf409bfec35b6ceb
Store Room <udchstore@ud.local>
2018-05-10 14:43:59 -0400

    1st commit

---
+++ notes.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+
+console.log('Starting notes.js');
+
+var fetchNotes = () => {
+    try{
+        var notesString = fs.readFileSync('notes-data.json');
+        return notes = JSON.parse(notesString);
+    } catch (e) {
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+
+    
+    
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+    
+};
+
+var getAll = () => {
+    console.log('Gettting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Reading note', title);
+}
+
+var removeNote = (title) => {
+        var notes = fetchNotes();
+        var filteredNotes = notes.filter((note) => {
+            if(note.title !== title) {
+                return note;
+            }
+        });
+        saveNotes(filteredNotes);
+        return notes.length !== filteredNotes.length;
+}
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
