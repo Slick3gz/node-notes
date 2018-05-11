ce69b66af5bd8904678d6ea1f9e4e6fc0bcfefd7
Store Room <udchstore@ud.local>
2018-05-11 09:50:10 -0400

    Added yargs .command(), title & body Options objects.

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
 
-const argv = yargs.argv;
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+}
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions 
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
+
 var command = argv._[0];
 
 
